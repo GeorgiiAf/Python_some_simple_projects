d80864cbddc7074f75c19eea7b02c1c3880ed077
GeorgeAf <gosha3484@gmail.com>
2024-12-18 12:15:03 +0200

    creating crud app

--- wb149/app.py
+++ wb149/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, send_from_directory, abort
+from flask import Flask, request, jsonify, send_from_directory, abort, render_template
+from flask_sqlalchemy import SQLAlchemy
 import csv
 import os
 
@@ -12,9 +13,10 @@ def read_csv_file(filename):
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')  # Отправляем HTML файл
+    return render_template('index.html')
 
-@app.route('/static')
+
+@app.route('/<path:filename>')
 def static_files(filename):
     if os.path.exists(filename):
         return send_from_directory('.', filename)  # Отправляем статический файл
@@ -35,6 +37,37 @@ def get_students():
         response_data = [student for student in students_data if student['фамилия'].lower() == surname]
 
     return jsonify(response_data)
+
+@app.route('/add_student', methods=['POST'])
+def add_student():
+    data = request.get_json()  # Получаем новые данные
+    new_student = Student(name=data['name'], surname=data['surname'])
+    db.session.add(new_student)
+    db.session.commit()
+    return jsonify({"message": "Student added successfully!"}), 201
+
+@app.route('/get_students', methods=['GET'])
+def get_students():
+    students_data = Student.query.all()  # Получаем всех студентов из базы данных
+    return jsonify([student.to_dict() for student in students_data])  # Возвращаем список студентов
+
+@app.route('/edit_student/<int:id>', methods=['PUT'])
+def edit_student(id):
+    data = request.get_json()  # Получаем новые данные
+    student = Student.query.get_or_404(id)  # Ищем студента по ID
+    student.name = data['name']
+    student.surname = data['surname']
+    db.session.commit()
+    return jsonify({"message": "Student updated successfully!"})
+
+@app.route('/delete_student/<int:id>', methods=['DELETE'])
+def delete_student(id):
+    student = Student.query.get_or_404(id)  # Ищем студента по ID
+    db.session.delete(student)
+    db.session.commit()
+    return jsonify({"message": "Student deleted successfully!"})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
